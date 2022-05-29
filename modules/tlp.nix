c179092b119cb2a4d6713a3392a45e229ecf8dad
Grigory Shipunov <blame@oxapentane.com>
2022-05-29 05:00:18 +0200

    tlp: update to new options

--- modules/tlp.nix
+++ modules/tlp.nix
@@ -3,8 +3,10 @@
 {
   powerManagement.cpuFreqGovernor = null;
   services.power-profiles-daemon.enable = false;
-  services.tlp.enable = true;
-  services.tlp.extraConfig = ''
-USB_BLACKLIST="1d50:604b 1d50:6089 1d50:cc15 1fc9:000c"
-'';
+  services.tlp ={
+    enable = true;
+    settings = {
+      USB_BLACKLIST="1d50:604b 1d50:6089 1d50:cc15 1fc9:000c";
+    };
+  };
 }
