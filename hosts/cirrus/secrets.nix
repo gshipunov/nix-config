b31fd6617f2b8ca1f05dc4fee4b1abb9f93c4d38
Grigory Shipunov <blame@oxapentane.com>
2022-06-17 19:28:55 +0200

    add cirrus secrets

---
+++ hosts/cirrus/secrets.nix
@@ -0,0 +1,9 @@
+{ config, ... }:
+{
+  sops.defaultSopsFile = ../../secrets/cirrus/secrets.yaml;
+  sops.age.sshKeyPaths = [ "/etc/ssh/ssh_host_ed25519_key" ];
+
+  sops.secrets = {
+    "wg/oxalab-seckey" = { };
+  };
+}
