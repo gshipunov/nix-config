7fbac2bdc49ff2c63acbec59589675bad9d27911
Grigory Shipunov <blame@oxapentane.com>
2022-06-17 03:02:37 +0200

    cirrus: fix host config

--- hosts/cirrus/configuration.nix
+++ hosts/cirrus/configuration.nix
@@ -25,12 +25,6 @@ zramSwap = { enable = true; algorithm = "zstd"; };
   # Select internationalisation properties.
   i18n.defaultLocale = "en_US.UTF-8";
 
-  environment.systemPackages = with pkgs; [
-    neovim
-    git
-    tmux
-  ];
-
   # Enable the OpenSSH daemon.
   users.users.root.openssh.authorizedKeys.keys = [
     "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAACAQDP6xE2ey0C8XXfvniiiHiqXsCC277jKI9RXEA+s2LQLUI5zl7v350i3Oa8H3NCcPj39lfMreqE6ncxcOhqYyzahPrrMkOqgbPAoRvq8H3ophLK+56O3xdHoKwLBwRD1yoGACjqG4UTiTrmnN2ateENgYcnTEY1e4vDw1qMj1drUXCsZ/6mkBBmHJiFfCaR4yCMt1r4gGi/dAC7ifnBP3oSyV/lJEwPxYYkGlbOBIvX/7Ar98pJS6xYPB3jHs9gwyNNON63d0fNYrwBojXPPCnGGaRZNOkBTzex3zZYp12ThINQ2xl8tRp9D8qpZ7vrLjhTD6AXkOBRzmDj+NsCeEaeTuWajqUM93iKncYUI+JxR1t7q8gA2pBMFzLesMXnx7R+5Kw7QDtSJM7a4GMIfsocPwf64BH6rzxEz68rXFE3P+J77PPM9CuaYw90JXHo3z220zYw2nMQ/1qjATVZw/hiVrLmQMVfmFJIufnGjTBs2sy3IoNyzvYm/oDeNNg1cdSV9gyyRKZhK08fxjXN5GSf9vZkfZa9tHtqaZ99HI40GQBHUVx1K2/NQJY8TVTSA+v16SFnJK8BIbmp/WFCuvDcMkgLIbqiYtDASe7P2mKIib86uOENT+P820egeLiTQ06kFw/gfUa8t69d5qEcjiQZ+lxCeYIs/E9KrEXHvRUWew== cardno:16 811 348"
@@ -41,14 +35,6 @@ zramSwap = { enable = true; algorithm = "zstd"; };
     enable = true;
     permitRootLogin = "prohibit-password";
   };
-
-  nix = {
-    package = pkgs.nixUnstable;
-    autoOptimiseStore = true;
-    extraOptions = ''
-      experimental-features = nix-command flakes
-      '';
-    };
 
   # Open ports in the firewall.
   networking.firewall.enable = true;
