1f8a3ff581a5733b2c04178a3db28c3dd43d2a16
Grigory Shipunov <blame@oxapentane.com>
2022-07-18 01:05:56 +0200

    use performance scaling on ac

--- modules/tlp.nix
+++ modules/tlp.nix
@@ -7,6 +7,8 @@
     enable = true;
     settings = {
       USB_BLACKLIST = "1d50:604b 1d50:6089 1d50:cc15 1fc9:000c";
+      CPU_SCALING_GOVERNOR_ON_AC = "performance";
+      CPU_SCALING_GOVERNOR_ON_BAT = "powersave";
     };
   };
 }
