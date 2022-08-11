2644fd3234e37927e6df9384d65f696bd6fddcdb
Grigory Shipunov <blame@oxapentane.com>
2022-08-12 01:04:16 +0200

    msmtp seems to be broken, moved back to .files

--- modules/mail/default.nix
+++ modules/mail/default.nix
@@ -27,41 +27,8 @@ in
   programs.msmtp = {
     enable = true;
     setSendmail = true;
-    defaults = {
-        tls_trust_file = "/etc/ssl/certs/ca-certificates.crt";
-        logfile = "/tmp/msmtp.log";
-    };
-    accounts = {
-      "mail@oxapentane.com" = {
-        host = "smtp.migadu.com";
-        port = 578;
-        from = "*@oxapentane.com";
-        user = "mail@oxapentane.com";
-        passwordeval = "cat ${config.sops.secrets."mail/oxapentane.com".path}";
-        auth = "on";
-        tls = "on";
-      };
-      "grigory@shipunov.xyz" = {
-        host = "smtp.migadu.com";
-        port = 578;
-        from = "*@shipunov.xyz";
-        user = "mail@oxapentane.com";
-        passwordeval = "cat ${config.sops.secrets."mail/shipunov.xyz".path}";
-        auth = "on";
-        tls = "on";
-      };
-      "dump@dvb.solutions" = {
-        host = "smtp.migadu.com";
-        port = 578;
-        from = "dump@dvb.solutions";
-        user = "dump@dvb.solutions";
-        passwordeval = "cat ${config.sops.secrets."mail/dvb.solutions".path}";
-        auth = "on";
-        tls = "on";
-      };
-    };
   };
-systemd.user = {
+  systemd.user = {
 
     services.mbsync = {
       enable = true;
