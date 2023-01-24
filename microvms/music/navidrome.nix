bcd0cf4e69f3b8b7f755131c52cf97b58b6c509a
Grigory Shipunov <blame@oxapentane.com>
2023-01-24 21:40:53 +0100

    open port

--- microvms/music/navidrome.nix
+++ microvms/music/navidrome.nix
@@ -1,10 +1,12 @@
 { config, ... }: {
+  networking.firewall.interfaces.oxaproxy.allowedTCPPorts = [ 4533 ];
   services.navidrome = {
     enable = true;
     settings = {
       BaseUrl = "/";
       EnableExternalServices = false;
       MusicFolder = "/var/lib/navidrome/library";
+      Port = 4533;
       ScanSchedule = "@every 17min";
       TranscondigCacheSize = "5GiB";
     };
