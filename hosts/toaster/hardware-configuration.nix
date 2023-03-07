f1495b288d73115e07012eb4866f360b83004cab
Grigory Shipunov <blame@oxapentane.com>
2023-03-07 17:24:20 +0100

    remove fprintd

--- hosts/toaster/hardware-configuration.nix
+++ hosts/toaster/hardware-configuration.nix
@@ -88,4 +88,6 @@
 
   nixpkgs.hostPlatform = lib.mkDefault "x86_64-linux";
   hardware.cpu.amd.updateMicrocode = lib.mkDefault config.hardware.enableRedistributableFirmware;
+
+  # services.fprintd.enable = true;
 }
