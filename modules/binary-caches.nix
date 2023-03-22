c12eefe43999f7d51bac61a43c0b7748622ae636
Grigory Shipunov <blame@oxapentane.com>
2023-03-22 21:47:31 +0100

    add nix-serve.hq.c3d2.de, tlm-solutions.cachix.org

--- modules/binary-caches.nix
+++ modules/binary-caches.nix
@@ -1,4 +1,4 @@
-{ pkgs, lib, ... }: {
+{ ... }: {
   nix = {
     extraOptions = ''
       builders-use-substitutes = true
@@ -11,20 +11,23 @@
       ];
       substituters = [
         "https://microvm.cachix.org"
-        # "https://hydra.hq.c3d2.de"
+        "https://nix-serve.hq.c3d2.de"
         "https://dump-dvb.cachix.org"
+        "https://tlm-solutions.cachix.org"
         "https://nix-community.cachix.org"
       ];
       trusted-substituters = [
         "https://microvm.cachix.org"
-        # "https://hydra.hq.c3d2.de"
+        "https://nix-serve.hq.c3d2.de"
         "https://dump-dvb.cachix.org"
+        "https://tlm-solutions.cachix.org"
         "https://nix-community.cachix.org"
       ];
       trusted-public-keys = [
         "microvm.cachix.org-1:oXnBc6hRE3eX5rSYdRyMYXnfzcCxC7yKPTbZXALsqys="
-        # "nix-serve.hq.c3d2.de:KZRGGnwOYzys6pxgM8jlur36RmkJQ/y8y62e52fj1ps="
+        "nix-serve.hq.c3d2.de:KZRGGnwOYzys6pxgM8jlur36RmkJQ/y8y62e52fj1ps="
         "dump-dvb.cachix.org-1:+Dq7gqpQG4YlLA2X3xJsG1v3BrlUGGpVtUKWk0dTyUU="
+        "tlm-solutions.cachix.org-1:J7qT6AvoNWPSj+59ed5bNESj35DLJNaROqga1EjVIoA="
         "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
       ];
     };
