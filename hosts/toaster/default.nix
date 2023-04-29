60d3e011995f874f0bfb06e17824d20323d29ee4
Grigory Shipunov <blame@oxapentane.com>
2023-04-29 23:50:51 +0200

    add factorio

--- hosts/toaster/default.nix
+++ hosts/toaster/default.nix
@@ -44,6 +44,10 @@
 
   programs.steam.enable = true;
 
+  environment.systemPackages = with pkgs; [
+    factorio
+  ];
+
   # This value determines the NixOS release from which the default
   # settings for stateful data, like file locations and database versions
   # on your system were taken. It‘s perfectly fine and recommended to leave
