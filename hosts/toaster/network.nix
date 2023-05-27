822ab6d257fcbce77a6cb8f89d56eb2f37bccef7
Grigory Shipunov <blame@oxapentane.com>
2023-05-27 22:38:53 +0200

    add networkmanager group

--- hosts/toaster/network.nix
+++ hosts/toaster/network.nix
@@ -1,5 +1,5 @@
-{ config, pkgs, ... }: {
-  environment.systemPackages = with pkgs; [ iwgtk ];
+{ pkgs, ... }: {
+  users.users.grue.extraGroups = [ "networkmanager" ];
 
   networking = {
     hostName = "toaster";
