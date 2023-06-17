0f8f1a7843a7ee88ca628b745218950a0fdfbdd4
Grigory Shipunov <blame@oxapentane.com>
2023-06-17 15:12:52 +0200

    fix the /boot

--- hosts/noctilucent/hardware-configuration.nix
+++ hosts/noctilucent/hardware-configuration.nix
@@ -1,8 +1,25 @@
-{ modulesPath, ... }:
+{ modulesPath, lib, ... }:
 {
   imports = [ (modulesPath + "/profiles/qemu-guest.nix") ];
   boot.loader.grub.device = "/dev/sda";
   boot.initrd.availableKernelModules = [ "ata_piix" "uhci_hcd" "xen_blkfront" "vmw_pvscsi" ];
   boot.initrd.kernelModules = [ "nvme" ];
-  fileSystems."/" = { device = "/dev/sda1"; fsType = "ext4"; };
+
+  fileSystems."/" = {
+    device = "/dev/disk/by-uuid/627952eb-107a-43c3-8223-bfea9af92837";
+    fsType = "ext4";
+  };
+  fileSystems."/boot" = {
+    device = "dev/disk/by-uuid/5CA6-CCE4";
+    fsType = "vfat";
+  };
+
+  swapDevices = [ ];
+
+  # Enables DHCP on each ethernet and wireless interface. In case of scripted networking
+  # (the default) this is the recommended approach. When using systemd-networkd it's
+  # still possible to use this option, but it's recommended to use it in conjunction
+  # with explicit per-interface declarations with `networking.interfaces.<interface>.useDHCP`.
+  networking.useDHCP = lib.mkDefault false;
+  networking.interfaces.enp1s0.useDHCP = lib.mkDefault false;
 }
