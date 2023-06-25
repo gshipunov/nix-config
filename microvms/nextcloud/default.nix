08c46d85dbb2b7439139522702c2eb20db80b515
Grigory Shipunov <blame@oxapentane.com>
2023-06-25 15:42:26 +0200

    update nextcloud to 27

--- microvms/nextcloud/default.nix
+++ microvms/nextcloud/default.nix
@@ -52,7 +52,7 @@
     enable = true;
     hostName = "nc.oxapentane.com";
     home = "/var/lib/nextcloud-oxa";
-    package = pkgs.nextcloud26;
+    package = pkgs.nextcloud27;
     maxUploadSize = "5000M";
     caching.redis = true;
     autoUpdateApps = {
