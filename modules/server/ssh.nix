57dca23048f7a785a96a92578eb7afa3bf3fb9fd
Grigory Shipunov <blame@oxapentane.com>
2023-08-05 21:06:23 +0200

    kick out root from ssh

--- modules/server/ssh.nix
+++ modules/server/ssh.nix
@@ -3,12 +3,9 @@
   programs.mosh.enable = true;
   services.openssh = {
     enable = true;
-    settings.PermitRootLogin = "prohibit-password";
+    settings.PermitRootLogin = "no";
+    settings.PasswordAuthentication = false;
   };
 
   networking.firewall.allowedTCPPorts = [ 22 ];
-  users.users.root.openssh.authorizedKeys.keys = [
-    "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIHv82n6F6kwJ3/EMYlOoCc1/NaYFW7QHC5F8jKVzdlio gshipunov@toaster"
-    "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIA3to/h8Myn+zXAkjboaRVqOfmtDz7VpIHhHbaRoYyPX g.shipunov@uva.nl"
-  ];
 }
