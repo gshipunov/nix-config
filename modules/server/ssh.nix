a0b0a553269067c705bc5bdf8bfe582ef9b156d9
Grigory Shipunov <blame@oxapentane.com>
2023-08-24 19:54:14 +0200

    add new ssh key

--- modules/server/ssh.nix
+++ modules/server/ssh.nix
@@ -8,4 +8,9 @@
   };
 
   networking.firewall.allowedTCPPorts = [ 22 ];
+  users.users.root.openssh.authorizedKeys.keys = [
+    "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIHv82n6F6kwJ3/EMYlOoCc1/NaYFW7QHC5F8jKVzdlio gshipunov@toaster"
+    "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIA3to/h8Myn+zXAkjboaRVqOfmtDz7VpIHhHbaRoYyPX g.shipunov@uva.nl"
+    "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIAlRhs/h3zTfonh/8wTZPEHnLAWXtedpF3ulJOwDbTGL owner@pixel"
+  ];
 }
