33830bf8db59741df2213093b48c6c8473e6253e
Grigory Shipunov <blame@oxapentane.com>
2023-08-25 09:14:29 +0200

    open eternal terminal port

--- modules/server/ssh.nix
+++ modules/server/ssh.nix
@@ -8,7 +8,7 @@
     settings.PasswordAuthentication = false;
   };
 
-  networking.firewall.allowedTCPPorts = [ 22 ];
+  networking.firewall.allowedTCPPorts = [ 22 2022 ];
   users.users.root.openssh.authorizedKeys.keys = [
     "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIHv82n6F6kwJ3/EMYlOoCc1/NaYFW7QHC5F8jKVzdlio gshipunov@toaster"
     "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIA3to/h8Myn+zXAkjboaRVqOfmtDz7VpIHhHbaRoYyPX g.shipunov@uva.nl"
