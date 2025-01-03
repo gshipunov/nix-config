a31fff4a8ee01555b8d24af5816b436998b74200
Grisha Shipunov <blame@oxapentane.com>
2025-01-03 13:50:53 +0100

    disable wifi powersaving

--- modules/tlp.nix
+++ modules/tlp.nix
@@ -9,6 +9,7 @@
       USB_BLACKLIST = "1d50:604b 1d50:6089 1d50:cc15 1fc9:000c";
       CPU_SCALING_GOVERNOR_ON_AC = "performance";
       CPU_SCALING_GOVERNOR_ON_BAT = "powersave";
+      WIFI_PWR_ON_BAT= "off";
     };
   };
 }
