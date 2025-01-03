6b472b8b0272f23808f5cd7db7b22474b1eaee7a
Grigory Shipunov <blame@oxapentane.com>
2025-01-03 23:31:05 +0000

    remove eternal-terminal

--- modules/server/ssh.nix
+++ modules/server/ssh.nix
@@ -1,6 +1,5 @@
 { ... }:
 {
-  services.eternal-terminal.enable = true;
   programs.mosh.enable = true;
   services.openssh = {
     enable = true;
@@ -8,7 +7,7 @@
     settings.PasswordAuthentication = false;
   };
 
-  networking.firewall.allowedTCPPorts = [ 22 2020 ];
+  networking.firewall.allowedTCPPorts = [ 22 ];
   users.users.root.openssh.authorizedKeys.keys = [
     "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIJl9iYG5oHBq/poBn7Jf1/FGWWbAnbx+NKjs7qtT3uAK 0xa@toaster 2024-12-31"
   ];
