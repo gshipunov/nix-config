130192786f65dd75281f2a00d87c46f83a8de8a9
Grigory Shipunov <blame@oxapentane.com>
2025-01-04 18:24:41 +0000

    cloud: now on networkd

--- hosts/cloud/networking.nix
+++ hosts/cloud/networking.nix
@@ -1,33 +1,34 @@
 { lib, ... }: {
-  # This file was populated at runtime with the networking
-  # details gathered from the active system.
-  networking = {
-    nameservers = [ "2a01:4ff:ff00::add:1"
- "2a01:4ff:ff00::add:2"
- "185.12.64.1"
- ];
-    defaultGateway = "172.31.1.1";
-    defaultGateway6 = {
-      address = "fe80::1";
-      interface = "eth0";
-    };
-    dhcpcd.enable = false;
-    usePredictableInterfaceNames = lib.mkForce false;
-    interfaces = {
-      eth0 = {
-        ipv4.addresses = [
-          { address="188.245.196.27"; prefixLength=32; }
+  networking.dhcpcd.enable = false;
+
+  networking.useNetworkd = true;
+  systemd.network.enable = true;
+  systemd.network = {
+    networks."30-uplink" = {
+      matchConfig.Name="enp1s0";
+      networkConfig = {
+        Address = [
+          "188.245.196.27/32"
+          "2a01:4f8:c17:7f8a::1/64"
         ];
-        ipv6.addresses = [
-          { address="2a01:4f8:c17:7f8a::1"; prefixLength=64; }
-{ address="fe80::9400:3ff:fef6:132d"; prefixLength=64; }
+        DNS = [
+          "2a01:4ff:ff00::add:1"
+          "2a01:4ff:ff00::add:2"
+          "185.12.64.1"
         ];
-        ipv4.routes = [ { address = "172.31.1.1"; prefixLength = 32; } ];
-        ipv6.routes = [ { address = "fe80::1"; prefixLength = 128; } ];
       };
+      routes = [
+        {
+          Gateway = "172.31.1.1";
+          GatewayOnLink = true;
+          Destination = "0.0.0.0/0";
+        }
+        {
+          Gateway = "fe80::1";
+          GatewayOnLink = true;
+          Destination = "::/0";
+        }
+      ];
     };
   };
-  services.udev.extraRules = ''
-    ATTR{address}=="96:00:03:f6:13:2d", NAME="eth0"
-  '';
 }
